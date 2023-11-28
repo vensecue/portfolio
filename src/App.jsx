168d6a956c47f756bfd4d23d5baee8b7c70d489c
Piotr Dzialo <piotr.sluzbowy@wp.pl>
2023-11-28 14:23:56 +0100

    siema test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
-  // // return (
+  // return (
   //   <BrowserRouter>
   //     <div className='relative z-0 bg-primary'>
   //       <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
@@ -22,7 +22,7 @@ const App = () => {
   //     </div>
   //   </BrowserRouter>
   // );
-  return <p>siema</p>
+  return <p>siemano</p>
 }
 
 export default App;
